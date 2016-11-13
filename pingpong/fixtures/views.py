3cfb12d0f9a005e82e5337f0ece0a9aa27111c1c
riemann742 <iwriemann742@gmail.com>
2016-11-13 20:28:46 +0900

    modify views and detail.html template

--- pingpong/fixtures/views.py
+++ pingpong/fixtures/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 from fixtures.models import *
 
 
-# Create your views here.
+# viewとは関係なし
+def createmixture(onegame):  # templateを楽にわかりやすく書くための関数
+    mixedls = []
+    for game in onegame:
+        p1, p2 = game.player_A, game.player_B
+        p1set, p2set = game.A_win_set, game.B_win_set
+        p1gains, p2gains = get_ABgain_from_onegame(game, "A"), get_ABgain_from_onegame(game, "B")
+        A_win_question = game.is_A_win
+
+        # [((A, Aセット), (B, Bセット)), (Aゲインリスト, Bゲインリスト), A勝ちか]
+        mixedls.append([((p1, p1set), (p2, p2set)), (p1gains, p2gains), A_win_question])
+    return mixedls
+
+
+def get_ABgain_from_onegame(game, letter):
+    filter_ = SetTable.objects.filter
+    dct = {"A": [x.A_gain for x in filter_(game_No=game)], "B": [x.B_gain for x in filter_(game_No=game)]}
+    return dct[letter]
+
+
+# 以下views
 def index(request):
     return render(request, "fixtures/index.html", {
         "league": League.objects.all(),
@@ -10,6 +30,7 @@ def index(request):
 
 
 def detail(request, pk):
+    # 以下データベース更新
     for g in OneGame.objects.all():
         g.calc_set_num()
         g.save()
@@ -17,15 +38,20 @@ def detail(request, pk):
         p.calc_gain_num()
         p.calc_various_num()
         p.save()
+    # 更新ここまで
+
     league = League.objects.get(pk=pk)
     participants = Participants.objects.filter(league=league)
     onegame = OneGame.objects.filter(league=league)
+    mixedls = createmixture(onegame)
     settable = [SetTable.objects.filter(game_No=og) for og in onegame]
+
     return render(request, "fixtures/detail.html", {
         "players": [x.player for x in participants],
         "league": league,
         "onegame": onegame,
-        "settable": settable,
+        "mixedls": mixedls,
+        "gameset": sorted([x for x in SetTable.objects.all()], key=lambda x: x.set_No),
     })
 
 

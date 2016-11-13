b7b4f327408ff94357e0ac17a1db9649da5623b8
riemann742 <iwriemann742@gmail.com>
2016-11-13 12:08:57 +0900

    First commit

--- pingpong/fixtures/tests.py
+++ pingpong/fixtures/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from fixtures.models import *
+
 
 # Create your tests here.
+class TestChange(TestCase):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        for game in OneGame.objects.all():
+            game.calc_set_num()
+        self.p = Player.objects.get(p_name="あ")
+        self.p.calc_gain_num()
+        self.p.calc_various_num()
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_change_playerdata(self):
+        self.assertEqual(self.p.total_win_points, 35)
+
+    def test_set_num(self):
+        ws = 0
+        ls = 0
+        for game in OneGame.objects.filter(player_A=self.p.pk):
+            ws += game.A_win_set
+            ls += game.B_win_set
+        for game in OneGame.objects.filter(player_B=self.p.pk):
+            ws += game.B_win_set
+            ls += game.A_win_set
+
+        self.assertEqual(ws, 2)
+        self.assertEqual(ls, 1)
--- pingpong/fixtures/views.py
+++ pingpong/fixtures/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from fixtures.models import *
+
 
 # Create your views here.
+def index(request):
+    return render(request, "fixtures/index.html", {
+        "league": League.objects.all(),
+    })
+
+
+def detail(request, pk):
+    for g in OneGame.objects.all():
+        g.calc_set_num()
+        g.save()
+    for p in Player.objects.all():
+        p.calc_gain_num()
+        p.calc_various_num()
+        p.save()
+    league = League.objects.get(pk=pk)
+    participants = Participants.objects.filter(league=league)
+    onegame = OneGame.objects.filter(league=league)
+    settable = [SetTable.objects.filter(game_No=og) for og in onegame]
+    return render(request, "fixtures/detail.html", {
+        "players": [x.player for x in participants],
+        "league": league,
+        "onegame": onegame,
+        "settable": settable,
+    })
+
+
+def regist(request):
+    return render(request, "fixtures/league_regist.html")
